827c8ecd10315d37cdd6e5e4d492fface5cc6ec7
Rustdj <81041717+Rustdj@users.noreply.github.com>
2022-03-03 21:10:40 +0300

    keep making cards

--- src/components/carts/cartLayaut.jsx
+++ src/components/carts/cartLayaut.jsx
@@ -1,7 +1,53 @@
 import React from "react";
 import c from "./cart.module.css";
+import Rating from '@mui/material/Rating';
+import PropTypes from 'prop-types';
+import ava from "../ava.svg";
+import SentimentVeryDissatisfiedIcon from '@mui/icons-material/SentimentVeryDissatisfied';
+import SentimentDissatisfiedIcon from '@mui/icons-material/SentimentDissatisfied';
+import SentimentSatisfiedIcon from '@mui/icons-material/SentimentSatisfied';
+import SentimentSatisfiedAltIcon from '@mui/icons-material/SentimentSatisfiedAltOutlined';
+import SentimentVerySatisfiedIcon from '@mui/icons-material/SentimentVerySatisfied';
+import classes from "../content/sorting/sorting.module.css";
 
 export default function CartLayaut() {
+
+  //rating-smile----------------------------
+
+  const customIcons = {
+    1: {
+      icon: <SentimentVeryDissatisfiedIcon />,
+      label: 'Very Dissatisfied',
+    },
+    2: {
+      icon: <SentimentDissatisfiedIcon />,
+      label: 'Dissatisfied',
+    },
+    3: {
+      icon: <SentimentSatisfiedIcon />,
+      label: 'Neutral',
+    },
+    4: {
+      icon: <SentimentSatisfiedAltIcon />,
+      label: 'Satisfied',
+    },
+    5: {
+      icon: <SentimentVerySatisfiedIcon />,
+      label: 'Very Satisfied',
+    },
+  };
+
+  function IconContainer(props) {
+    const { value, ...other } = props;
+    return <span {...other}>{customIcons[value].icon}</span>;
+  }
+
+  IconContainer.propTypes = {
+    value: PropTypes.number.isRequired,
+  };
+  
+  //rating-smile-------------------------
+
   return (
     <div className={c.doctorContainer}>
       <div className={c.wrapper}>
@@ -9,11 +55,16 @@ export default function CartLayaut() {
           <div className={c.pageAvatar}>
             <a className={c.avatarLink} href="">
               <div className={c.avaPhoto}>
-                <img src="" alt="" />
+                <img src={ava} alt="ava" />
               </div>
             </a>
             <div title="Добавить врача в избранное" className={c.buttonLike}>
-              <img src="" alt="" />
+              <Rating
+                name="highlight-selected-only"
+                defaultValue={2}
+                IconContainerComponent={IconContainer}
+                highlightSelectedOnly
+              />
             </div>
           </div>
           <a className={c.starValue} href="">
@@ -25,8 +76,8 @@ export default function CartLayaut() {
               </div>
               <div className={c.recommendations}>
                 <div className={c.value}>
-                    <span>81% пациентов</span>
-                    <span>рекомендует врача</span>
+                  <span>81% пациентов</span>
+                  <span>рекомендует врача</span>
                 </div>
                 <span className={c.valueLabel}>83 отзыва</span>
               </div>
@@ -84,7 +135,77 @@ export default function CartLayaut() {
             </div>
             <div className={c.infoPhone}></div>
           </div>
-          <div className={c.pageCatdClinic}></div>
+          <div className={c.pageCatdClinic}>
+            <div className={c.clinicInfo}>
+              <div className={c.clinicAppointments}>
+                <div className={c.wrapper}>
+                  <div className={c.items}>
+                    <div className={c.itemCaption}>
+                      Выберите время приёма для записи онлайн
+                    </div>
+                    <div className={c.slotsDays}>
+                      <div className={c.slotsBtns}>
+                        <button className={classes.filterButton}>
+                          четверг,3
+                        </button>
+                        <button className={classes.filterBtns}>
+                          пятница,4
+                        </button>
+                        <button className={classes.filterBtn}>суббота,5</button>
+                      </div>
+                    </div>
+                    <div className={c.btnBottom}>
+                      <button className={c.time}>19:30</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className={c.clinicLocation}>
+                <div className={c.addressBlock}>
+                  <div className={c.clinicName}></div>
+                  <div className={c.clinicAdress}></div>
+                  <div className={c.clinicStations}>
+                    <div className={c.clinicStation}>
+                      <a className={c.metroStation} href="">
+                        <div className={c.label}>
+                          <span className={c.icon}></span>
+                          <span className={c.name}>Кропоткинская</span>
+                        </div>
+                        <div className={c.dist}>
+                          <img className={c.svgIcon} src="" alt="" />
+                          <span>550 м</span>
+                        </div>
+                      </a>
+                    </div>
+                    <div className={c.clinicStation}>
+                      <a className={c.metroStation} href="">
+                        <div className={c.label}>
+                          <span className={c.icon}></span>
+                          <span className={c.name}>Арбатская</span>
+                        </div>
+                        <div className={c.dist}>
+                          <img className={c.svgIcon} src="" alt="" />
+                          <span>940 м</span>
+                        </div>
+                      </a>
+                    </div>
+                    <div className={c.clinicStation}>
+                      <a className={c.metroStation} href="">
+                        <div className={c.label}>
+                          <span className={c.icon}></span>
+                          <span className={c.name}>Смоленская</span>
+                        </div>
+                        <div className={c.dist}>
+                          <img className={c.svgIcon} src="" alt="" />
+                          <span>820 м</span>
+                        </div>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </section>
       </div>
     </div>
